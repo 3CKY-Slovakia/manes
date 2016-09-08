c131e9a86e8cf1fe5e2a9336ee2b691db3a20908
jkmetko <j.kmetko@gmail.com>
2016-09-08 15:49:36 +0200

    Pridanie novych textov, odstranenie nedoplnenych sekcii

--- resources/views/practice-areas_legal_companies.blade.php
+++ resources/views/practice-areas_legal_companies.blade.php
@@ -38,7 +38,7 @@
                         <div class="Title-area">
                             <h3>Služby pre</h3>
                             <h2>Právnicke osoby</h2>
-                            <p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut molestie ipsum. Duis nec odio malesuada, tincidunt dui sed, finibus urna. Donec vitae sem eget lectus pulvinar ultrices at nec ipsum. Pellentesque aliquam, arcu sed vestibulum molestie, metus nulla faucibus nisi, feugiat euismod dolor erat eu odio. Sed gravida enim tristique est gravida.</p>
+                            <p>V oblasti správy pohľadávok prichádzame do kontaktu s právnickými osobami rôznych zameraní a služieb, preto ku každému klientovi pristupujeme individuálne a naše služby prispôsobujeme klientom na mieru podľa ich potrieb. </p>
                         </div>
                     </div>
                 </div>
@@ -99,58 +99,58 @@
      START PRACTICE SECTION
     ============================== -->
 
-    <section class="practice-area home-2-practice-area type-1-bg">
-        <div class="welcome-title-area">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12 col-sm-12">
-                        <div class="Title-area">
-                            <h3>Všetky oblasti</h3>
-                            <h2>Ďalšie služby</h2>
-                            <p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut molestie ipsum. Duis nec odio malesuada, tincidunt dui sed, finibus urna. Donec vitae sem eget lectus pulvinar ultrices at nec ipsum. Pellentesque aliquam, arcu sed vestibulum molestie, metus nulla faucibus nisi, feugiat euismod dolor erat eu odio. Sed gravida enim tristique est gravida.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> <!-- end title. it will use all pages title -->
+    {{--<section class="practice-area home-2-practice-area type-1-bg">--}}
+        {{--<div class="welcome-title-area">--}}
+            {{--<div class="container">--}}
+                {{--<div class="row">--}}
+                    {{--<div class="col-xs-12 col-sm-12">--}}
+                        {{--<div class="Title-area">--}}
+                            {{--<h3>Všetky oblasti</h3>--}}
+                            {{--<h2>Ďalšie služby</h2>--}}
+                            {{--<p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut molestie ipsum. Duis nec odio malesuada, tincidunt dui sed, finibus urna. Donec vitae sem eget lectus pulvinar ultrices at nec ipsum. Pellentesque aliquam, arcu sed vestibulum molestie, metus nulla faucibus nisi, feugiat euismod dolor erat eu odio. Sed gravida enim tristique est gravida.</p>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div> <!-- end title. it will use all pages title -->--}}
 
-        <!-- start practice content area -->
-        <div class="container">
-            <div class="row">
-                <div class="h-2-practice-content clearfix">
-                    <div class="col-sm-4 col-md-4">
-                        <div class="h-2-p-c-details">
-                            <div class="h-2-p-c-default">
-                                <i class="fa fa-legal"></i>
-                                <h2><a href="{{ url('sluzby/pravnicke-osoby') }}">Právnické osoby</a></h2>
-                                <p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut molestie ipsum. Duis nec odio malesuada, tincidunt dui sed, finibus urna.</p>
-                                <a href="{{ url('sluzby/pravnicke-osoby') }}">Viac <i class="fa fa-long-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-4 col-md-4">
-                        <div class="h-2-p-c-details">
-                            <div class="h-2-p-c-default">
-                                <i class="fa  fa-male"></i>
-                                <h2><a href="{{ url('sluzby/fyzicke-osoby') }}">Fyzické osoby</a></h2>
-                                <p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut molestie ipsum. Duis nec odio malesuada, tincidunt dui sed, finibus urna.</p>
-                                <a href="{{ url('sluzby/fyzicke-osoby') }}">Viac <i class="fa fa-long-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-4 col-md-4">
-                        <div class="h-2-p-c-details">
-                            <div class="h-2-p-c-default">
-                                <i class="fa fa-info"></i>
-                                <h2><a href="{{ url('sluzby/doplnkove-sluzby') }}">Doplnkové služby</a></h2>
-                                <p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut molestie ipsum. Duis nec odio malesuada, tincidunt dui sed, finibus urna.</p>
-                                <a href="{{ url('sluzby/doplnkove-sluzby') }}">Viac <i class="fa fa-long-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section> <!-- end practice area -->
+        {{--<!-- start practice content area -->--}}
+        {{--<div class="container">--}}
+            {{--<div class="row">--}}
+                {{--<div class="h-2-practice-content clearfix">--}}
+                    {{--<div class="col-sm-4 col-md-4">--}}
+                        {{--<div class="h-2-p-c-details">--}}
+                            {{--<div class="h-2-p-c-default">--}}
+                                {{--<i class="fa fa-legal"></i>--}}
+                                {{--<h2><a href="{{ url('sluzby/pravnicke-osoby') }}">Právnické osoby</a></h2>--}}
+                                {{--<p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut molestie ipsum. Duis nec odio malesuada, tincidunt dui sed, finibus urna.</p>--}}
+                                {{--<a href="{{ url('sluzby/pravnicke-osoby') }}">Viac <i class="fa fa-long-arrow-right"></i></a>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    {{--<div class="col-sm-4 col-md-4">--}}
+                        {{--<div class="h-2-p-c-details">--}}
+                            {{--<div class="h-2-p-c-default">--}}
+                                {{--<i class="fa  fa-male"></i>--}}
+                                {{--<h2><a href="{{ url('sluzby/fyzicke-osoby') }}">Fyzické osoby</a></h2>--}}
+                                {{--<p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut molestie ipsum. Duis nec odio malesuada, tincidunt dui sed, finibus urna.</p>--}}
+                                {{--<a href="{{ url('sluzby/fyzicke-osoby') }}">Viac <i class="fa fa-long-arrow-right"></i></a>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    {{--<div class="col-sm-4 col-md-4">--}}
+                        {{--<div class="h-2-p-c-details">--}}
+                            {{--<div class="h-2-p-c-default">--}}
+                                {{--<i class="fa fa-info"></i>--}}
+                                {{--<h2><a href="{{ url('sluzby/doplnkove-sluzby') }}">Doplnkové služby</a></h2>--}}
+                                {{--<p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut molestie ipsum. Duis nec odio malesuada, tincidunt dui sed, finibus urna.</p>--}}
+                                {{--<a href="{{ url('sluzby/doplnkove-sluzby') }}">Viac <i class="fa fa-long-arrow-right"></i></a>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</section> <!-- end practice area -->--}}
 
 @endsection
--- resources/views/practice-areas_persons.blade.php
+++ resources/views/practice-areas_persons.blade.php
@@ -38,7 +38,7 @@
                         <div class="Title-area">
                             <h3>Služby pre</h3>
                             <h2>Fyzické osoby</h2>
-                            <p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut molestie ipsum. Duis nec odio malesuada, tincidunt dui sed, finibus urna. Donec vitae sem eget lectus pulvinar ultrices at nec ipsum. Pellentesque aliquam, arcu sed vestibulum molestie, metus nulla faucibus nisi, feugiat euismod dolor erat eu odio. Sed gravida enim tristique est gravida.</p>
+                            <p>Fyzickým osobám sa naša spoločnosť venuje v projekte e-inkaso, kde si klienti prostredníctvom internetovej stránky www.einkaso.sk vložia svoju pohľadávku do našej správy bezplatne a pohodlne prostredníctvom internetu. </p>
                         </div>
                     </div>
                 </div>
@@ -73,58 +73,58 @@
      START PRACTICE SECTION
     ============================== -->
 
-    <section class="practice-area home-2-practice-area type-1-bg">
-        <div class="welcome-title-area">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12 col-sm-12">
-                        <div class="Title-area">
-                            <h3>Všetky oblasti</h3>
-                            <h2>Ďalšie služby</h2>
-                            <p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut molestie ipsum. Duis nec odio malesuada, tincidunt dui sed, finibus urna. Donec vitae sem eget lectus pulvinar ultrices at nec ipsum. Pellentesque aliquam, arcu sed vestibulum molestie, metus nulla faucibus nisi, feugiat euismod dolor erat eu odio. Sed gravida enim tristique est gravida.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> <!-- end title. it will use all pages title -->
+    {{--<section class="practice-area home-2-practice-area type-1-bg">--}}
+        {{--<div class="welcome-title-area">--}}
+            {{--<div class="container">--}}
+                {{--<div class="row">--}}
+                    {{--<div class="col-xs-12 col-sm-12">--}}
+                        {{--<div class="Title-area">--}}
+                            {{--<h3>Všetky oblasti</h3>--}}
+                            {{--<h2>Ďalšie služby</h2>--}}
+                            {{--<p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut molestie ipsum. Duis nec odio malesuada, tincidunt dui sed, finibus urna. Donec vitae sem eget lectus pulvinar ultrices at nec ipsum. Pellentesque aliquam, arcu sed vestibulum molestie, metus nulla faucibus nisi, feugiat euismod dolor erat eu odio. Sed gravida enim tristique est gravida.</p>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div> <!-- end title. it will use all pages title -->--}}
 
-        <!-- start practice content area -->
-        <div class="container">
-            <div class="row">
-                <div class="h-2-practice-content clearfix">
-                    <div class="col-sm-4 col-md-4">
-                        <div class="h-2-p-c-details">
-                            <div class="h-2-p-c-default">
-                                <i class="fa fa-legal"></i>
-                                <h2><a href="{{ url('sluzby/pravnicke-osoby') }}">Právnické osoby</a></h2>
-                                <p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut molestie ipsum. Duis nec odio malesuada, tincidunt dui sed, finibus urna.</p>
-                                <a href="{{ url('sluzby/pravnicke-osoby') }}">Viac <i class="fa fa-long-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-4 col-md-4">
-                        <div class="h-2-p-c-details">
-                            <div class="h-2-p-c-default">
-                                <i class="fa  fa-male"></i>
-                                <h2><a href="{{ url('sluzby/fyzicke-osoby') }}">Fyzické osoby</a></h2>
-                                <p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut molestie ipsum. Duis nec odio malesuada, tincidunt dui sed, finibus urna.</p>
-                                <a href="{{ url('sluzby/fyzicke-osoby') }}">Viac <i class="fa fa-long-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-4 col-md-4">
-                        <div class="h-2-p-c-details">
-                            <div class="h-2-p-c-default">
-                                <i class="fa fa-info"></i>
-                                <h2><a href="{{ url('sluzby/doplnkove-sluzby') }}">Doplnkové služby</a></h2>
-                                <p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut molestie ipsum. Duis nec odio malesuada, tincidunt dui sed, finibus urna.</p>
-                                <a href="{{ url('sluzby/doplnkove-sluzby') }}">Viac <i class="fa fa-long-arrow-right"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section> <!-- end practice area -->
+        {{--<!-- start practice content area -->--}}
+        {{--<div class="container">--}}
+            {{--<div class="row">--}}
+                {{--<div class="h-2-practice-content clearfix">--}}
+                    {{--<div class="col-sm-4 col-md-4">--}}
+                        {{--<div class="h-2-p-c-details">--}}
+                            {{--<div class="h-2-p-c-default">--}}
+                                {{--<i class="fa fa-legal"></i>--}}
+                                {{--<h2><a href="{{ url('sluzby/pravnicke-osoby') }}">Právnické osoby</a></h2>--}}
+                                {{--<p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut molestie ipsum. Duis nec odio malesuada, tincidunt dui sed, finibus urna.</p>--}}
+                                {{--<a href="{{ url('sluzby/pravnicke-osoby') }}">Viac <i class="fa fa-long-arrow-right"></i></a>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    {{--<div class="col-sm-4 col-md-4">--}}
+                        {{--<div class="h-2-p-c-details">--}}
+                            {{--<div class="h-2-p-c-default">--}}
+                                {{--<i class="fa  fa-male"></i>--}}
+                                {{--<h2><a href="{{ url('sluzby/fyzicke-osoby') }}">Fyzické osoby</a></h2>--}}
+                                {{--<p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut molestie ipsum. Duis nec odio malesuada, tincidunt dui sed, finibus urna.</p>--}}
+                                {{--<a href="{{ url('sluzby/fyzicke-osoby') }}">Viac <i class="fa fa-long-arrow-right"></i></a>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    {{--<div class="col-sm-4 col-md-4">--}}
+                        {{--<div class="h-2-p-c-details">--}}
+                            {{--<div class="h-2-p-c-default">--}}
+                                {{--<i class="fa fa-info"></i>--}}
+                                {{--<h2><a href="{{ url('sluzby/doplnkove-sluzby') }}">Doplnkové služby</a></h2>--}}
+                                {{--<p class="red">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut molestie ipsum. Duis nec odio malesuada, tincidunt dui sed, finibus urna.</p>--}}
+                                {{--<a href="{{ url('sluzby/doplnkove-sluzby') }}">Viac <i class="fa fa-long-arrow-right"></i></a>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</section> <!-- end practice area -->--}}
 
 @endsection
